1b2194623deb01389ac952adba42e75733204afd
Leonardo Murillo <leonardo@murillodigital.com>
2013-01-25 13:16:35 -0600

    more experimentation

--- js/experimental.js
+++ js/experimental.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -15,8 +15,8 @@ camera.position.z = 5;
 
 function render() {
 	requestAnimationFrame(render);
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.05;
+	cube.rotation.x += 0.001;
+	cube.rotation.y += 0.005;
 	renderer.render(scene, camera);
 } 
 
