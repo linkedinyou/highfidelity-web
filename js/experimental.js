6f26a5d50d007b9c6e18a1e706d3a87c7c72a029
Leonardo Murillo <leonardo@murillodigital.com>
2013-01-25 12:46:12 -0600

    Some arrangements

--- js/experimental.js
+++ js/experimental.js
@@ -7,12 +7,16 @@ var geometry = new THREE.CubeGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({
 	color : 0x00ff00
 });
+
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 5;
+
 function render() {
 	requestAnimationFrame(render);
 	cube.rotation.x += 0.1;
 	cube.rotation.y += 0.1;
 	renderer.render(scene, camera);
-} render(); 
+} 
+
+render(); 
