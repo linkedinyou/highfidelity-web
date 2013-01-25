4560cfd735ab77c1d3fb16b32a6981a7ef0f1863
Leonardo Murillo <leonardo@murillodigital.com>
2013-01-25 13:18:05 -0600

    more experimentation

--- js/experimental.js
+++ js/experimental.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(500, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
